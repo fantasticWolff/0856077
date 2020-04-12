0ef86cf6aba7aa83ddbd829af8cb593c16c34edc
fantasticWolff <fantastic1936@gmail.com>
2020-04-12 22:05:19 +0800

    Update TestRunner.java

--- TestRunner.java
+++ TestRunner.java
@@ -1,7 +1,9 @@
 public class TestRunner {
     public static void main(String args[]) {
-        PriorityQueueTest pqt = new PriorityQueueTest(new Integer[]{3, 1, 2}, new Integer[]{1, 2, 4});
-
+        //Fail
+        //PriorityQueueTest pqt = new PriorityQueueTest(new Integer[]{3, 1, 2}, new Integer[]{1, 2, 4});
+        //Pass
+        PriorityQueueTest pqt = new PriorityQueueTest(new Integer[]{3, 1, 2}, new Integer[]{1, 2, 3});
         pqt.sum();
     }
 }
