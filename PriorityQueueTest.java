f2faad286b401fee11077fb1be8ad342cd619d98
fantasticWolff <fantastic1936@gmail.com>
2020-04-12 11:42:20 +0800

    Update PriorityQueueTest.java

--- PriorityQueueTest.java
+++ PriorityQueueTest.java
@@ -12,53 +12,53 @@ import org.junit.runners.Parameterized.Parameters;
 
 import static org.junit.Assert.assertEquals;
 
-public static void main(String args[]) {
-    @RunWith(Parameterized.class)
-    public class PriorityQueueTest {
-        PriorityQueue pq = new PriorityQueue<>();
 
-        Integer[] input;
-        Integer[] expect;
-        public PriorityQueueTest(Integer[] input, Integer[] expect){
-            this.input = input;
-            this.expect = expect;
-        }
+@RunWith(Parameterized.class)
+public class PriorityQueueTest {
+    PriorityQueue pq = new PriorityQueue<>();
 
-        @Parameters
-        public static Collection compare(){
-            return Arrays.asList(new Integer[][][]
-            {
-                { {3, 1, 2}, {1, 2, 3} },
-                { {1, 1, 2}, {1, 1, 2} },
-                { {3, 0, 2}, {0, 2, 3} },
-                { {11, 1, 2}, {1, 2, 11} },
-                { {3, -1, 2}, {-1, 2, 3} }
-            });
-        }
+    Integer[] input;
+    Integer[] expect;
+    public PriorityQueueTest(Integer[] input, Integer[] expect){
+        this.input = input;
+        this.expect = expect;
+    }
 
-        @Test
-        public void sum(){
-            pq.addAll(Arrays.asList(input));
-            for(int i=0; i<input.length; i++){
-                assertEquals(pq.poll(), expect[i]);
-            }
-        }
+    @Parameters
+    public static Collection compare(){
+        return Arrays.asList(new Integer[][][]
+        {
+            { {3, 1, 2}, {1, 2, 3} },
+            { {1, 1, 2}, {1, 1, 2} },
+            { {3, 0, 2}, {0, 2, 3} },
+            { {11, 1, 2}, {1, 2, 11} },
+            { {3, -1, 2}, {-1, 2, 3} }
+        });
+    }
 
-        @Test(expected = NullPointerException.class)
-        public void e1(){
-            pq.addAll(null);
-        }
-
-        Integer[] a1;
-        @Test(expected = NullPointerException.class)
-        public void e2(){
-            //Integer[] a1 = {null};
-            PriorityQueue pq2 = new PriorityQueue(Arrays.asList(a1));
-        }
-
-        @Test(expected = IllegalArgumentException.class)
-        public void e3(){
-            pq.addAll(pq);
+    @Test
+    public void sum(){
+        pq.addAll(Arrays.asList(input));
+        for(int i=0; i<input.length; i++){
+            assertEquals(pq.poll(), expect[i]);
         }
     }
+
+    @Test(expected = NullPointerException.class)
+    public void e1(){
+        pq.addAll(null);
+    }
+
+    Integer[] a1;
+    @Test(expected = NullPointerException.class)
+    public void e2(){
+        //Integer[] a1 = {null};
+        PriorityQueue pq2 = new PriorityQueue(Arrays.asList(a1));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void e3(){
+        pq.addAll(pq);
+    }
 }
+
